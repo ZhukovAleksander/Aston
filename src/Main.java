b05e754767fef4e6999040e5d39a762a748111d2
ZhukovAleksander <zhukov.alexunder.2002@gmail.com>
2025-07-05 23:33:41 +0300

    Lesson_5

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 class Main {
+    static class MyArraySizeException extends Exception {}
+
     public static void main(String[] args) {
-        ArrExeption(4, 4);
+        ArrExeption();
     }
 
     public static void ArrExeption() {
-        int[][] arr = new int[a][b];
+        int[][] arr = new int[4][4];
+        try {
+            if (arr.length > 4 | arr.length < 4) {}
+        }
+        catch (MyArraySizeException e) {
+            System.out.println(e);
+        }
     }
+
 }
