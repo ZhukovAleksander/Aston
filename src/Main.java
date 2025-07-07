cff507c8e72914da99c3bdccca8bec3300d65bee
ZhukovAleksander <zhukov.alexunder.2002@gmail.com>
2025-07-07 21:55:06 +0300

    Lesson_3.3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
+
 public class Main {
     public static void main(String[] args) {
-        Product[] productsArray = new Product[5];
-        productsArray[0] = new Product("имя1", "дата1", "производитель1", "страна1", 1, true);
-        productsArray[1] = new Product("имя2", "дата2", "производитель2", "страна2", 2, true);
-        productsArray[2] = new Product("имя3", "дата3", "производитель3", "страна3", 3, false);
-        productsArray[3] = new Product("имя4", "дата4", "производитель4", "страна4", 4, false);
-        productsArray[4] = new Product("имя5", "дата5", "производитель5", "страна5", 5, false);
+        Park park = new Park();
 
-        for (int i = 0; i < productsArray.length; i++) {
-            productsArray[i].printInfo();
-        }
+        Park.Attraction attraction1 = park.new Attraction("Аттракцион 1", "10:00-22:00", 500);
+        Park.Attraction attraction2 = park.new Attraction("Аттракцион 2", "12:00-22:00", 800);
+        Park.Attraction attraction3 = park.new Attraction("Аттракцион 3", "13:00-22:00", 300);
+
+        attraction1.printInfo();
+        attraction2.printInfo();
+        attraction3.printInfo();
     }
 }
