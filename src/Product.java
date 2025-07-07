752f6aa04a8a798b84b31f93dfd79deef4015e21
ZhukovAleksander <zhukov.alexunder.2002@gmail.com>
2025-07-07 22:01:16 +0300

    Lesson_3

--- src/Product.java
+++ src/Product.java
@@ -1,10 +1,10 @@
 class Product {
-    String name;
-    String productionDate;
-    String manufacturer;
-    String country;
-    double price;
-    boolean isReserved;
+    private String name;
+    private String productionDate;
+    private String manufacturer;
+    private String country;
+    private double price;
+    private boolean isReserved;
 
     public Product(String name, String productionDate, String manufacturer,
                    String country, double price, boolean isReserved) {
@@ -16,13 +16,37 @@ class Product {
         this.isReserved = isReserved;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getProductionDate() {
+        return productionDate;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isReserved() {
+        return isReserved;
+    }
+
     public void printInfo() {
-        System.out.println("Название: " + name);
-        System.out.println("Дата производства: " + productionDate);
-        System.out.println("Производитель: " + manufacturer);
-        System.out.println("Страна происхождения: " + country);
-        System.out.println("Цена: " + price + " руб.");
-        System.out.println("Забронирован: " + (isReserved ? "Да" : "Нет"));
+        System.out.println("Название: " + getName());
+        System.out.println("Дата производства: " + getProductionDate());
+        System.out.println("Производитель: " + getManufacturer());
+        System.out.println("Страна происхождения: " + getCountry());
+        System.out.println("Цена: " + getPrice() + " руб.");
+        System.out.println("Забронирован: " + (isReserved() ? "Да" : "Нет"));
         System.out.println();
     }
 }
